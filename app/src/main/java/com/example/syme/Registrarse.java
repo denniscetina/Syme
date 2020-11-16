99367aa2522db5a17e5a7208df71e88b59486ea4
denniscetina <44214500+denniscetina@users.noreply.github.com>
2020-11-15 19:56:35 -0500

    Login listo Corrección de colores aun no esta

--- app/src/main/java/com/example/syme/Registrarse.java
+++ app/src/main/java/com/example/syme/Registrarse.java
@@ -1,13 +1,13 @@
 package com.example.syme;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
